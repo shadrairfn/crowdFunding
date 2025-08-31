840112c7ddf85456dc23673a099cbec064274eb6
Shadra <irfanshadra7@gmail.com>
2025-08-31 09:43:11 +0700

    creating verification sign up user using email otp

--- src/routes/verificationRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import { sendOtpEmail, verifyOtpEmail } from "../controllers/verificationController.js"
-
-const router = Router()
-
-router.route("/send-otp").post(sendOtpEmail)
-router.route("/verify-otp").post(verifyOtpEmail)
-
-export default router
